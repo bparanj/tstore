6767dec7404e204a501567565e281d3e2d22570e
Bala Paranj <bparanj@gmail.com>
2013-02-03 10:49:27 -0800

    Style selected tab as active

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,13 @@
 module ApplicationHelper
+  def active?(page_name)
+    # logger.info("Page name #{page_name}")
+    # logger.info("Controller name #{params[:controller]}")
+    servlet = params[:controller]
+    if servlet == page_name
+      "active"
+    else
+      ""
+    end
+  end
 
 end
