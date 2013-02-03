3fc19c16be54657e270825ce316b32a30001340c
Bala Paranj <bparanj@gmail.com>
2013-02-03 02:44:16 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tstore::Application.config.secret_token = '1671ca328d4ed619a590b3632a0ae8829dc7710c6bee8ff5fd03fdcc10e49cc066d91f211815d2d2c9dd974d336c6dc26920e19c2594989b5a941078b29d18a3'
